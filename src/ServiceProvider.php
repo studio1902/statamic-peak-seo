91806645b033d21b8ea29c050ae70ac76244b496
Marco Rieser <marco@rieser.dev>
2023-02-03 23:14:55 +0100

    Initialize SEO addon

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Studio1902\PeakSeo;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
