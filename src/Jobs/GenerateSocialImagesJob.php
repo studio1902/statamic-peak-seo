59e4af482f13901b0ea0a648ea5c6a5960677b41
Sander Goorden <72132494+SanderGo@users.noreply.github.com>
2023-06-28 19:15:35 +0200

    Adds check for s3 driver when saving Browsershot (#17)

--- src/Jobs/GenerateSocialImagesJob.php
+++ src/Jobs/GenerateSocialImagesJob.php
@@ -73,8 +73,12 @@ class GenerateSocialImagesJob implements ShouldQueue
         $image = Browsershot::url("{$absolute_url}/social-images/{$id}")
             ->windowSize(1200, 630)
             ->select('#og')
-            ->waitUntilNetworkIdle()
-            ->save($disk->path($file));
+            ->waitUntilNetworkIdle();
+        if (strtolower(config("filesystems.disks.{$container->disk}.driver") == 's3')) {
+            $disk->put($file, $image->screenshot());
+        } else {
+            $image->save($disk->path($file));
+        }
         $container->makeAsset($file)->save();
         $this->item->set('og_image', $file)->save();
 
