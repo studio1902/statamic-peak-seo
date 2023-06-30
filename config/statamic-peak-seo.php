e93ec9108df62716097ddc0fb163041a6273ea7e
Marco Rieser <marco@rieser.dev>
2023-06-30 19:40:03 +0200

    match env var name with config var name

--- config/statamic-peak-seo.php
+++ config/statamic-peak-seo.php
@@ -4,6 +4,6 @@ return [
     'social_image' => [
         'node_binary' => env('SOCIAL_IMAGE_NODE_BINARY'),
         'npm_binary' => env('SOCIAL_IMAGE_NPM_BINARY'),
-        'queue_name' => env('SOCIAL_IMAGE_QUEUE', 'default')
+        'queue_name' => env('SOCIAL_IMAGE_QUEUE_NAME', 'default')
     ],
 ];
