28dd3d6eecc710c81a6c99346a5d79d41a9af15f
Rob de Kort <rob@studio1902.nl>
2023-09-21 15:15:59 +0200

    Only update globals if fields don't exist
    In update script

--- src/Updates/AddCookieNotice.php
+++ src/Updates/AddCookieNotice.php
@@ -16,67 +16,73 @@ class AddCookieNotice extends UpdateScript
     {
         $blueprint = GlobalSet::findByHandle('configuration')->blueprint();
         $contents = $blueprint->contents();
-        $newSection = [
-            'display' => 'Cookie notice',
-            'instructions' => 'Configure an optional cookie notice.',
-            'fields' => [
-            0 => [
-                'handle' => 'cookie_notice_type',
-                'field' => [
-                'options' => [
-                    'none' => 'None',
-                    'entry' => 'Entry',
-                    'pdf' => 'PDF',
-                ],
-                'default' => 'none',
-                'type' => 'button_group',
-                'instructions' => 'This will be used in form consent and in the optional cookie banner.',
-                'instructions_position' => 'below',
-                'listable' => false,
-                'localizable' => true,
-                'display' => 'Cookie notice',
-                'width' => 50,
-                ],
-            ],
-            1 => [
-                'handle' => 'cookie_notice_asset',
-                'field' => 'common.image',
-                'config' => [
-                'container' => 'files',
-                'localizable' => true,
-                'listable' => 'hidden',
-                'display' => 'Cookie notice (PDF)',
-                'width' => 50,
-                'if' => [
-                    'cookie_notice_type' => 'equals pdf',
-                ],
-                'validate' => [
-                    0 => 'required_if:cookie_notice_type,pdf',
-                ],
-                ],
-            ],
-            2 => [
-                'handle' => 'cookie_notice_entry',
-                'field' => 'common.entry',
-                'config' => [
-                'localizable' => true,
-                'listable' => 'hidden',
-                'display' => 'Cookie notice (entry)',
-                'width' => 50,
-                'if' => [
-                    'cookie_notice_type' => 'equals entry',
-                ],
-                'validate' => [
-                    0 => 'required_if:cookie_notice_type,entry',
-                ],
-                ],
-            ],
-            ],
-        ];
-        $contents['tabs']['general']['sections'][] = $newSection;
-        $blueprint->setContents($contents);
-        $blueprint->save();
 
-        $this->console()->info('Global Configuration Blueprint succesfully expanded with Cookie Notice fields.');
+        if (collect($contents['tabs']['general']['sections'])->where('display', 'Cookie notice')->isEmpty()) {
+
+            $newSection = [
+                'display' => 'Cookie notice',
+                'instructions' => 'Configure an optional cookie notice.',
+                'fields' => [
+                0 => [
+                    'handle' => 'cookie_notice_type',
+                    'field' => [
+                    'options' => [
+                        'none' => 'None',
+                        'entry' => 'Entry',
+                        'pdf' => 'PDF',
+                    ],
+                    'default' => 'none',
+                    'type' => 'button_group',
+                    'instructions' => 'This will be used in form consent and in the optional cookie banner.',
+                    'instructions_position' => 'below',
+                    'listable' => false,
+                    'localizable' => true,
+                    'display' => 'Cookie notice',
+                    'width' => 50,
+                    ],
+                ],
+                1 => [
+                    'handle' => 'cookie_notice_asset',
+                    'field' => 'common.image',
+                    'config' => [
+                    'container' => 'files',
+                    'localizable' => true,
+                    'listable' => 'hidden',
+                    'display' => 'Cookie notice (PDF)',
+                    'width' => 50,
+                    'if' => [
+                        'cookie_notice_type' => 'equals pdf',
+                    ],
+                    'validate' => [
+                        0 => 'required_if:cookie_notice_type,pdf',
+                    ],
+                    ],
+                ],
+                2 => [
+                    'handle' => 'cookie_notice_entry',
+                    'field' => 'common.entry',
+                    'config' => [
+                    'localizable' => true,
+                    'listable' => 'hidden',
+                    'display' => 'Cookie notice (entry)',
+                    'width' => 50,
+                    'if' => [
+                        'cookie_notice_type' => 'equals entry',
+                    ],
+                    'validate' => [
+                        0 => 'required_if:cookie_notice_type,entry',
+                    ],
+                    ],
+                ],
+                ],
+            ];
+            $contents['tabs']['general']['sections'][] = $newSection;
+            $blueprint->setContents($contents);
+            $blueprint->save();
+
+            $this->console()->info('Global Configuration Blueprint succesfully expanded with Cookie Notice fields.');
+        } else {
+            $this->console()->info('Global Configuration Blueprint already contains Cookie Notice fields.');
+        }
     }
 }
