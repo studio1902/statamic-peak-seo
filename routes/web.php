61667e300dfb34e86c40057e173415478b7639fb
Rob de Kort <rob@studio1902.nl>
2023-11-13 16:07:05 +0100

    Prevent loop

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,23 @@ if ($useSitemap) {
 }
 
 // Register routes for each site.
-Site::all()->each(function (\Statamic\Sites\Site $site) use ($useSitemap, $useSocialImageGeneration) {
-    $relativeSiteUrl = URL::makeRelative($site->url());
+if ($useSitemap || $useSocialImageGeneration) {
+    Site::all()->each(function (\Statamic\Sites\Site $site) use ($useSitemap, $useSocialImageGeneration) {
+        $relativeSiteUrl = URL::makeRelative($site->url());
 
-    // The Sitemap route.
-    if ($useSitemap) {
-        Route::statamic(URL::tidy($relativeSiteUrl . '/sitemap.xml'), 'statamic-peak-seo::sitemap/sitemap', [
-            'layout' => null,
-            'content_type' => 'application/xml'
-        ]);
-    }
+        // The Sitemap route.
+        if ($useSitemap) {
+            Route::statamic(URL::tidy($relativeSiteUrl . '/sitemap.xml'), 'statamic-peak-seo::sitemap/sitemap', [
+                'layout' => null,
+                'content_type' => 'application/xml'
+            ]);
+        }
 
-    // The Social Image route to generate social images.
-    if ($useSocialImageGeneration) {
-        Route::statamic(URL::tidy($relativeSiteUrl . '/social-images/{id}'), 'statamic-peak-seo::social_images', [
-            'layout' => null,
-        ]);
-    }
-});
+        // The Social Image route to generate social images.
+        if ($useSocialImageGeneration) {
+            Route::statamic(URL::tidy($relativeSiteUrl . '/social-images/{id}'), 'statamic-peak-seo::social_images', [
+                'layout' => null,
+            ]);
+        }
+    });
+}
