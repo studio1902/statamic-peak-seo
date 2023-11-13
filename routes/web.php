f6fca686c4b2a52497a1f0d59fe7c474611e80b8
Rob de Kort <rob@studio1902.nl>
2023-11-13 15:59:40 +0100

    Make sitemap optional

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,28 @@ use Statamic\Facades\GlobalSet;
 use Statamic\Facades\Site;
 use Statamic\Facades\URL;
 
+$useSitemap = (bool)GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_sitemap');
 $useSocialImageGeneration = (bool)GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_social_image_generation');
 
 // The Sitemap Index route for listing sitemaps of all (multi)sites.
-Route::statamic('/sitemaps.xml', 'statamic-peak-seo::sitemap/sitemaps', [
-    'layout' => null,
-    'content_type' => 'application/xml'
-]);
-
-// Register routes for each site.
-Site::all()->each(function (\Statamic\Sites\Site $site) use ($useSocialImageGeneration) {
-    $relativeSiteUrl = URL::makeRelative($site->url());
-
-    // The Sitemap route.
-    Route::statamic(URL::tidy($relativeSiteUrl . '/sitemap.xml'), 'statamic-peak-seo::sitemap/sitemap', [
+if ($useSitemap) {
+    Route::statamic('/sitemaps.xml', 'statamic-peak-seo::sitemap/sitemaps', [
         'layout' => null,
         'content_type' => 'application/xml'
     ]);
+}
+
+// Register routes for each site.
+Site::all()->each(function (\Statamic\Sites\Site $site) use ($useSitemap, $useSocialImageGeneration) {
+    $relativeSiteUrl = URL::makeRelative($site->url());
+
+    // The Sitemap route.
+    if ($useSitemap) {
+        Route::statamic(URL::tidy($relativeSiteUrl . '/sitemap.xml'), 'statamic-peak-seo::sitemap/sitemap', [
+            'layout' => null,
+            'content_type' => 'application/xml'
+        ]);
+    }
 
     // The Social Image route to generate social images.
     if ($useSocialImageGeneration) {
