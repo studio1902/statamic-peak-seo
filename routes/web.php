359e95bbe7deb7bc8785e5b8a76fa9e1fbf4542d
Rob de Kort <rob@studio1902.nl>
2024-05-01 20:06:13 +0200

    Use closures for routes

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,47 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Statamic\Exceptions\NotFoundHttpException;
 use Statamic\Facades\GlobalSet;
 use Statamic\Facades\Site;
 use Statamic\Facades\URL;
 
-$useSitemap = false;
-$useSocialImageGeneration = false;
-
-try {
-    $useSitemap = (bool)GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_sitemap');
-    $useSocialImageGeneration = (bool)GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_social_image_generation');
-} catch (\Exception $e) {
-    // no action
-}
-
 // The Sitemap Index route for listing sitemaps of all (multi)sites.
-if ($useSitemap) {
-    Route::statamic('/sitemaps.xml', 'statamic-peak-seo::sitemap/sitemaps', [
+Route::statamic('/sitemaps.xml', 'statamic-peak-seo::sitemap/sitemaps', function () {
+    if (! GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_sitemap')) {
+        throw new NotFoundHttpException;
+    }
+
+    return [
         'layout' => null,
-        'content_type' => 'application/xml'
-    ]);
-}
+        'content_type' => 'application/xml',
+    ];
+});
 
 // Register routes for each site.
-if ($useSitemap || $useSocialImageGeneration) {
-    Site::all()->each(function (\Statamic\Sites\Site $site) use ($useSitemap, $useSocialImageGeneration) {
-        $relativeSiteUrl = URL::makeRelative($site->url());
+Site::all()->each(function (\Statamic\Sites\Site $site) {
+    $relativeSiteUrl = URL::makeRelative($site->url());
 
-        // The Sitemap route.
-        if ($useSitemap) {
-            Route::statamic(URL::tidy($relativeSiteUrl . '/sitemap.xml'), 'statamic-peak-seo::sitemap/sitemap', [
-                'layout' => null,
-                'content_type' => 'application/xml'
-            ]);
+    // The Sitemap route.
+    Route::statamic(URL::tidy($relativeSiteUrl.'/sitemap.xml'), 'statamic-peak-seo::sitemap/sitemap', function () {
+        if (! GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_sitemap')) {
+            throw new NotFoundHttpException;
         }
 
-        // The Social Image route to generate social images.
-        if ($useSocialImageGeneration) {
-            Route::statamic(URL::tidy($relativeSiteUrl . '/social-images/{id}'), 'statamic-peak-seo::social_images', [
-                'layout' => null,
-            ]);
-        }
+        return [
+            'layout' => null,
+            'content_type' => 'application/xml',
+        ];
     });
-}
+
+    // The Social Image route to generate social images.
+    Route::statamic(URL::tidy($relativeSiteUrl.'/social-images/{id}'), 'statamic-peak-seo::social_images', function ($id) {
+        if (! GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_social_image_generation')) {
+            throw new NotFoundHttpException;
+        }
+
+        return [
+            'layout' => null,
+        ];
+    });
+});
