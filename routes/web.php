7108d352c18d0c0cd8c9d8d2bff51e8bed7dfc95
Rob de Kort <rob@studio1902.nl>
2024-05-02 22:14:28 +0200

    Simplify routes

--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Statamic\Exceptions\NotFoundHttpException;
-use Statamic\Facades\GlobalSet;
-use Statamic\Facades\Site;
 use Statamic\Facades\URL;
 
 // The Sitemap Index route for listing sitemaps of all (multi)sites.
-Route::statamic('/sitemaps.xml', 'statamic-peak-seo::sitemap/sitemaps', function () {
-    if (! GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_sitemap')) {
-        throw new NotFoundHttpException;
-    }
+Route::statamic(URL::tidy('/sitemaps.xml'), 'statamic-peak-seo::sitemap/sitemaps', [
+    'layout' => null,
+    'content_type' => 'application/xml',
+]);
 
-    return [
-        'layout' => null,
-        'content_type' => 'application/xml',
-    ];
-});
+// The Sitemap routes.
+Route::statamic(URL::tidy('sitemap.xml'), 'statamic-peak-seo::sitemap/sitemap', [
+    'layout' => null,
+    'content_type' => 'application/xml',
+]);
 
-// Register routes for each site.
-Site::all()->each(function (\Statamic\Sites\Site $site) {
-    $relativeSiteUrl = URL::makeRelative($site->url());
+Route::statamic(URL::tidy('{current_site}/sitemap.xml'), 'statamic-peak-seo::sitemap/sitemap', [
+    'layout' => null,
+    'content_type' => 'application/xml',
+]);
 
-    // The Sitemap route.
-    Route::statamic(URL::tidy($relativeSiteUrl.'/sitemap.xml'), 'statamic-peak-seo::sitemap/sitemap', function () {
-        if (! GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_sitemap')) {
-            throw new NotFoundHttpException;
-        }
-
-        return [
-            'layout' => null,
-            'content_type' => 'application/xml',
-        ];
-    });
-
-    // The Social Image route to generate social images.
-    Route::statamic(URL::tidy($relativeSiteUrl.'/social-images/{id}'), 'statamic-peak-seo::social_images', function ($id) {
-        if (! GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_social_image_generation')) {
-            throw new NotFoundHttpException;
-        }
-
-        return [
-            'layout' => null,
-        ];
-    });
-});
+// The Social Image route to generate social images.
+Route::statamic(URL::tidy('{current_site}/social-images/{id}'), 'statamic-peak-seo::social_images', [
+    'layout' => null,
+]);
